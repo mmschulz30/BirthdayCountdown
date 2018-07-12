456bf90317e1e0ccf5cb4f187af85b61f9698c44
Makenna Schulz <mmschulz30@gmail.com>
2018-07-12 09:37:51 -0600

    added large text and remaining label. finished css front end

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
 import changeDate from './changeDate';
+import LargeText from './largeText';
 
 export default class App extends Component {
 
@@ -18,17 +19,19 @@ export default class App extends Component {
     if(this.state.active) {
       return [
         <Clock/>,
-        changeDate('Change Date', () => this.setState({ active: false }))
+        changeDate('Change Date', () => this.setState({ active: false })),
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining until your 21st Birthday</label>
       ]
     }else {
-      return Button('Generate Countdown', () => this.setState({active: true }))
+      return[
+        <Picker/>,
+        Button('Generate Countdown', () => this.setState({active: true }))
+      ]
     }
   }.bind(this)
 
   render() {
-
-    //return <div className="grid"><Clock/></div>
-
     return (
       <div className="grid">
       <h1 className="grid__title">Brithday Countdown</h1>
@@ -41,7 +44,6 @@ export default class App extends Component {
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
 
-        <Picker/>
         { this.renderItems() }
       </div>
     );
